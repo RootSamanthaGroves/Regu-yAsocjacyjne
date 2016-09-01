fc818af4c2cff8d11e95aa5dc5fc39eff7f47b5b
kosa1010 <kosa1010@o2.pl>
2016-09-01 11:04:47 +0200

    Revert "test2"
    This reverts commit a397745e6f35f27c54ccf6c533333c9a7dbf2184.

--- src/associativerules/Main.java
+++ src/associativerules/Main.java
@@ -14,7 +14,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         java.awt.EventQueue.invokeLater(() -> {
             new Windows.windowWEKA().setVisible(true);
-            //tt
         });
     }
 }
