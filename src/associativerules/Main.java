a397745e6f35f27c54ccf6c533333c9a7dbf2184
kosa1010 <kosa1010@o2.pl>
2016-09-01 11:01:12 +0200

    test2

--- src/associativerules/Main.java
+++ src/associativerules/Main.java
@@ -14,6 +14,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         java.awt.EventQueue.invokeLater(() -> {
             new Windows.windowWEKA().setVisible(true);
+            //tt
         });
     }
 }
